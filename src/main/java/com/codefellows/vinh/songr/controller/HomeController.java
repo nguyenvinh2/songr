1f9f6be45424704b9f29b844a05e8ac591c82d2d
Vinh Nguyen <nguyenv2@outlook.com>
2019-06-13 13:02:47 -0700

    updated readme

--- src/main/java/com/codefellows/vinh/songr/controller/HomeController.java
+++ src/main/java/com/codefellows/vinh/songr/controller/HomeController.java
@@ -12,16 +12,27 @@ public class HomeController {
 
     @Autowired
     AlbumRepository albums;
+    int count = 0;
 
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String index(Model model){
-        Album jonas = new Album("Happiness Begins", "Jonas Brothers",14,2623,"https://rovimusic.rovicorp.com/image.jpg?c=2P9-GJSqE8oq6reBtjbzogSijaXJlYnq0St31qpAJWo=&f=5");
-        Album dylan = new Album("The Rolling Thunder Revue: The 1975 Live Recordings","Bob Dylan",25,30345,"https://cdn-s3.allmusic.com/release-covers/500/0005/057/0005057564.jpg");
-        albums.save(jonas);
-        albums.save(dylan);
+        if (count == 0) {
+            Album jonas = new Album("Happiness Begins", "Jonas Brothers", 14, 2623, "https://rovimusic.rovicorp.com/image.jpg?c=2P9-GJSqE8oq6reBtjbzogSijaXJlYnq0St31qpAJWo=&f=5");
+            Album dylan = new Album("The Rolling Thunder Revue: The 1975 Live Recordings", "Bob Dylan", 25, 30345, "https://cdn-s3.allmusic.com/release-covers/500/0005/057/0005057564.jpg");
+            albums.save(jonas);
+            albums.save(dylan);
+            count++;
+        }
         Iterable<Album> songs = albums.findAll();
         model.addAttribute("albums", songs);
+        model.addAttribute("object", new Album());
         return "index";
+    }
+
+    @RequestMapping(value="/", method= RequestMethod.POST)
+    public String create(@ModelAttribute("object") Album input) {
+        albums.save(input);
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/codefellows/vinh/songr/model/Album.java
+++ src/main/java/com/codefellows/vinh/songr/model/Album.java
@@ -4,11 +4,60 @@ import javax.persistence.*;
 
 @Entity
 public class Album {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
     public String title;
     public String artist;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     public int songCount;
     public int length;
     public String imageUrl;
