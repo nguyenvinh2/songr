395e2496162d0331949dee1a075b9d68a4c37770
Vinh Nguyen <nguyenv2@outlook.com>
2019-06-13 16:05:51 -0700

    updated to include full crud

--- src/main/java/com/codefellows/vinh/songr/repository/AlbumRepository.java
+++ src/main/java/com/codefellows/vinh/songr/repository/AlbumRepository.java
@@ -3,5 +3,7 @@ package com.codefellows.vinh.songr.repository;
 import com.codefellows.vinh.songr.model.Album;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AlbumRepository extends CrudRepository<Album, Integer> {
 }
